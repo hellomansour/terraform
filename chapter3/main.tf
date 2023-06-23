919b69a899817739f10c52e12cdefa694fb219c8
hellomansour <mr.a.mansour@gmail.com>
2023-06-23 14:05:10 +0300

    Grammar fix

--- chapter3/main.tf
+++ chapter3/main.tf
@@ -35,7 +35,7 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
-  name = "first-bucket-12249901-lockss"
+  name = "first-bucket-12249901-locks"
   billing_mode = "PAY_PER_REQUEST"
   hash_key = "LockID"
   attribute {
